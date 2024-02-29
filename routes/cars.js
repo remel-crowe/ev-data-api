4e8d895cadba677090ffc3a0b5bfb5d32c6cf37a
remel-crowe <remelscrowe@gmail.com>
2024-02-29 11:26:40 +0000

    feat: endpoint added

--- routes/cars.js
+++ routes/cars.js
@@ -12,4 +12,30 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/brand/:brand", async (req, res) => {
+  const brand = req.params.brand;
+  try {
+    const cars = await Car.find({ Brand: brand });
+    res.json(cars);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// e.g. GET /brand/Audi
+
+router.get("/search", async (req, res) => {
+  const { brand, model } = req.query;
+  let query = {};
+  if (brand) query.Brand = brand;
+  if (model) query.Model = model;
+  try {
+    const cars = await Car.find(query);
+    res.json(cars);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// e.g. GET /search?brand=Audi&model=A3
+
 module.exports = router;
